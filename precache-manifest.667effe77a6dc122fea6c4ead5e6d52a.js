16d29e2f2c4dc8f2420fe70a098874787b3951f5
MuhammadSaberEl-Wazer <muhammadsaberit@gmail.com>
2022-01-01 22:13:45 +0200

    Updates

---
+++ precache-manifest.667effe77a6dc122fea6c4ead5e6d52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bba31ebe530bcc52b47",
+    "url": "/birthday-react-app/static/css/main.8a550513.chunk.css"
+  },
+  {
+    "revision": "1bba31ebe530bcc52b47",
+    "url": "/birthday-react-app/static/js/main.1bba31eb.chunk.js"
+  },
+  {
+    "revision": "492e6404043daf733d1d",
+    "url": "/birthday-react-app/static/js/1.492e6404.chunk.js"
+  },
+  {
+    "revision": "6be168b627ac2cf40e5d",
+    "url": "/birthday-react-app/static/js/runtime~main.6be168b6.js"
+  },
+  {
+    "revision": "e153d65f0e93a76a93039abc03fc991b",
+    "url": "/birthday-react-app/index.html"
+  }
+];
